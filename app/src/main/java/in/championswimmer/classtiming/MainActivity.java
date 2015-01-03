efa797afb62274c84c3db4e1ae627bc13ce41612
Arnav Gupta <championswimmer@gmail.com>
2015-01-03 18:31:30 +0530

    Initial commit

---
+++ app/src/main/java/in/championswimmer/classtiming/MainActivity.java
@@ -0,0 +1,54 @@
+package in.championswimmer.classtiming;
+
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+    public static final String TAG = "TimingTest";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "before anything" + SystemClock.uptimeMillis());
+        TimingTestOne tto1 = new TimingTestOne();
+        Log.d(TAG, "after first" + SystemClock.uptimeMillis());
+
+        TimingTestOne tto2 = new TimingTestOne();
+        Log.d(TAG, "after second" + SystemClock.uptimeMillis());
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/in/championswimmer/classtiming/TimingTestOne.java
@@ -0,0 +1,32 @@
+package in.championswimmer.classtiming;
+
+/**
+ * Created by championswimmer on 3/1/15.
+ */
+public class TimingTestOne {
+
+    int someFunction1 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+    int someFunction2 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+    int someFunction3 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+    int someFunction4 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+    int someFunction5 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+    int someFunction6 ( int one ) {
+        int two = 2 * one;
+        return two;
+    }
+}
